be5faf71daa702c51914002b1e12c1f6949c2a57
yigeon <yigeon84@gmail.com>
2015-03-20 01:06:48 +0900

    first Init

---
+++ app/src/test/java/com/momori/wepic/LoginActivityTest.java
@@ -0,0 +1,25 @@
+package com.momori.wepic;
+
+import android.test.suitebuilder.annotation.SmallTest;
+import junit.framework.TestCase;
+
+public class LoginActivityTest extends TestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        System.out.println("setUp");
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        System.out.println("tearDown");
+        super.tearDown();
+    }
+
+    @SmallTest
+    public void testAdd(){
+        assertEquals(null, null);
+        System.out.println("Test Case");
+    }
+}
