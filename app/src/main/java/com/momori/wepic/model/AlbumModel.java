910794fa33bfbe2a286a06c824e1866a94a2d98a
LEEMYEONGHYEON <mhyeon.lee@gmail.com>
2015-05-02 00:28:07 +0900

    Main 작업 중 앨범 리스트 데이터 가져와서 뿌리기

--- app/src/main/java/com/momori/wepic/model/AlbumModel.java
+++ app/src/main/java/com/momori/wepic/model/AlbumModel.java
@@ -1,16 +1,31 @@
 package com.momori.wepic.model;
 
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by sec on 2015-04-13.
  */
 public class AlbumModel {
     public static final String ALBUM_ID = "ALBUM_ID";
 
-    private String group_id;
-
     private String album_id;
 
     private String album_name;
+
+    private String album_img_url;
+
+    private String group_id;
+
+    private long total_photo_cnt;
+
+    private Date init_create_datetime;
+
+    private Date end_datetime;
+
+    private List<UserModel> user_info;
+
+
 
     public String getGroup_id() {
         return group_id;
@@ -34,5 +49,45 @@ public class AlbumModel {
 
     public void setAlbum_name(String album_name) {
         this.album_name = album_name;
+    }
+
+    public String getAlbum_img_url() {
+        return album_img_url;
+    }
+
+    public void setAlbum_img_url(String album_img_url) {
+        this.album_img_url = album_img_url;
+    }
+
+    public long getTotal_photo_cnt() {
+        return total_photo_cnt;
+    }
+
+    public void setTotal_photo_cnt(long total_photo_cnt) {
+        this.total_photo_cnt = total_photo_cnt;
+    }
+
+    public Date getInit_create_datetime() {
+        return init_create_datetime;
+    }
+
+    public void setInit_create_datetime(Date init_create_datetime) {
+        this.init_create_datetime = init_create_datetime;
+    }
+
+    public Date getEnd_datetime() {
+        return end_datetime;
+    }
+
+    public void setEnd_datetime(Date end_datetime) {
+        this.end_datetime = end_datetime;
+    }
+
+    public List<UserModel> getUser_info() {
+        return user_info;
+    }
+
+    public void setUser_info(List<UserModel> user_info) {
+        this.user_info = user_info;
     }
 }
---
+++ app/src/main/java/com/momori/wepic/model/AlbumUserModel.java
@@ -0,0 +1,17 @@
+package com.momori.wepic.model;
+
+/**
+ * Created by Hyeon on 2015-05-01.
+ */
+public class AlbumUserModel extends UserModel{
+
+    private String user_state;
+
+    public String getUser_state() {
+        return user_state;
+    }
+
+    public void setUser_state(String user_state) {
+        this.user_state = user_state;
+    }
+}
